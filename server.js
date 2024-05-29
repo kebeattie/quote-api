056063fc938d6734156b1269095dfac7b72ac53e
kebeattie <kebeattie98@hotmail.com>
2024-05-29 16:00:10 +0100

    Implemented GET all, GET by author, POST new quote API calls

--- server.js
+++ server.js
@@ -13,6 +13,39 @@ app.get('/api/quotes/random', (req, res, next) => {
     res.send({quote: quote});
 })
 
+app.get('/api/quotes', (req, res, next) => {
+    if (req.query.person) {
+        const person = req.query.person;
+        const quotesToSend = quotes.filter((quote) => 
+            quote.person === person
+        );
+        
+        res.send({
+                quotes: quotesToSend
+        });
+        
+    } else {
+        res.send({
+            quotes: quotes
+        })
+    }
+})
+
+app.post('/api/quotes', (req, res, next) => {
+    const { quote, person } = req.query;
+    if (quote && person) {
+        const newQuote = {
+            quote: quote,
+            person: person
+        }
+        quotes.push(newQuote)
+        res.send({quote: newQuote});
+    }
+    else {
+        res.status(400).send(newQuote);
+    }
+})
+
 app.listen(PORT, () => {
 
 });
